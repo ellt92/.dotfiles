2ccb009212796ef2a97db18237df75be17d9028e
Elliott Thompson <elliott@moo.com>
2016-06-30 11:05:18 +0100

    updated the linux install file

--- install_dotfiles_linux.sh
+++ install_dotfiles_linux.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+apt-get install git
+apt-get install zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s `which zsh`
 ln -s .dotfiles/.vimrc .vimrc
 ln -s .dotfiles/.zshrc .zshrc
 ln -s .dotfiles/.eslintrc .eslintrc
+ln -s .dotfiles/elliott.zsh-theme .oh-my-zsh/themes/elliott.zsh-theme

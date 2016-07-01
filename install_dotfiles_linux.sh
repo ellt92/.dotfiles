d356b4bdb7052e5227d4a73d779d5cfab1a097a3
Elliott Thompson <ellth92@gmail.com>
2016-07-01 09:15:32 -0400

    added youcompleteme to the install

--- install_dotfiles_linux.sh
+++ install_dotfiles_linux.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt-get install git
 apt-get install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
@@ -8,3 +7,7 @@ ln -s .dotfiles/.zshrc .zshrc
 ln -s .dotfiles/.eslintrc .eslintrc
 cd .oh-my-zsh/themes
 ln -s ../../.dotfiles/elliott.zsh-theme elliott.zsh-theme
+cd ~/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+apt-get install vim vim-youcompleteme
+vam install youcompleteme
